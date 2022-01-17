8fad6451b1902e19e872b1a71dd211298ff18c0f
zura-japoshvili <japoshvili777@gmail.com>
2022-01-17 20:43:39 +0400

    #2 - adding style.css file

--- index.php
+++ index.php
@@ -5,8 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RSS reader</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <form method="post">
+        <input type="text" name="feedval" placeholder="Please enter the website feed URL" class="feedval">
+        <input type="submit" class="sub-btn">
+    </form>
+    <?php 
+        $url = "";
+        if(isset($_POST['submit'])){
+            if($_POST['feedval'] != ''){
+                $url = $_POST['feedval'];
+                echo $url;
+            }else{
+                echo 123123;
+            }
+        }
+    ?>
 </body>
 </html>
