a1c8f93ce094bf332122472dc71b20ada00c4b7a
zura-japoshvili <japoshvili777@gmail.com>
2022-01-17 22:10:36 +0400

    #3 update(final)

--- index.php
+++ index.php
@@ -10,18 +10,51 @@
 <body>
     <form method="post">
         <input type="text" name="feedval" placeholder="Please enter the website feed URL" class="feedval">
-        <input type="submit" class="sub-btn">
+        <input type="submit" name="submit" class="sub-btn">
     </form>
     <?php 
-        $url = "";
+        $url = "https://makitweb.com/feed/";
         if(isset($_POST['submit'])){
             if($_POST['feedval'] != ''){
                 $url = $_POST['feedval'];
-                echo $url;
             }else{
-                echo 123123;
+                
             }
         }
+
+        if(@simplexml_load_file($url)){
+            $feeds = @simplexml_load_file($url);
+        }else{
+            echo "URL is invalid";
+        }
+
+        if(!empty($feeds)):
+            $site = $feeds->channel->title;
+            $siteLink = $feeds->channel->link;
+
+            echo "<h1 class='web-title'>". $site ."</h1>";
+
+            foreach($feeds->channel->item as $item):
+                $title = $item->title;
+                $link = $item->link;
+                $description = $item->description;
+                $postDate = $item->pubDate;
+                $pubDate = date('d M Y, D', strtotime($postDate));
+                
+            ?> 
+                <div class="post">
+                    <div class="post-head">
+                        <h2><a href="<?php echo $link ?>"><?php echo $title ?></a></h2>
+                        <p><?php echo $pubDate ?></p>
+                    </div>
+                    <div class="post-content">
+                        <p><?php echo implode(' ', array_slice(explode(' ', $description), 0, 20)) . "..."; ?> <a href="<?php echo $link; ?>">Read more</a></p>
+                    </div>
+                </div>
+    <?php    
+            endforeach;
+        endif;
     ?>
+
 </body>
 </html>
